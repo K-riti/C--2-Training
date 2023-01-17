3abc8299e29134dcdf860f1f719fd3d0a486619a
Kriti Bhaskar <Kriti.Bhaskar@clinisys.com>
2023-01-17 17:16:42 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace Practice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {    //if else
+             // string grade;
+             // System.Console.WriteLine("Enter Your marks :");
+             // int marks = Convert.ToInt32(Console.ReadLine());
+             // if (marks > 90)
+             // {
+             //     grade = "Outstanding";
+             //     System.Console.WriteLine(grade);
+             // }
+             // else if(marks>60&&marks<90)
+             // {
+             //     grade="Good";
+             //     System.Console.WriteLine(grade);
+             // }
+             // else if(marks>40&&marks<=60){
+             //     grade="Average";
+             //     System.Console.WriteLine(grade);
+
+            // }
+            // else{
+            //     System.Console.WriteLine("Fail");
+            // }
+
+            //for loop 
+
+            // for(int i=0;i<25;i++){
+            // if(i%3==0)
+            // System.Console.WriteLine(i);
+
+            // } 
+
+            //GOTO
+
+            // System.Console.WriteLine("France");
+            // System.Console.WriteLine("India");
+            // goto mylabel;
+            // System.Console.WriteLine("Europe");
+            // System.Console.WriteLine("China");
+            // System.Console.WriteLine("Korea");
+            // mylabel:
+            // System.Console.WriteLine("Nepal");
+            // System.Console.WriteLine("USA");
+            // System.Console.WriteLine("UK");
+            // System.Console.WriteLine("Brazil");
+
+            // System.Console.WriteLine("New Zealand");  
+
+            //FOREACH
+
+            // char[] myArray = {'H','e','l','l','o'};
+
+            // foreach(char ch in myArray)
+            // {
+            //     Console.Write(ch);
+            // }
+            // String student;
+            // System.Console.WriteLine("ENTER NAME OF STUDENTS ");
+            // string students1=Console.ReadLine();
+
+            // //break continue
+            // int a =15;
+            // for(int i=a;i>0;i--){
+            //     if(i%2==0){
+            //     //break;
+            //     continue;
+            //     }
+            //     System.Console.WriteLine(i);
+            //}
+            // int i=0;
+            // while(i<10){
+            //     System.Console.WriteLine(i);
+            //     i++;
+
+            // }
+            // do{
+            //     System.Console.WriteLine(i);
+            //     i++;
+            // }while(i<10);
+
+            // char grade;
+            // System.Console.WriteLine("enter your grade :");
+            // grade = Convert.ToChar(Console.Read());
+
+            // string gradeDescription;
+            // switch (grade)
+            // {
+            //     case 'O':
+            //         gradeDescription = "outstanding";
+            //         break;
+            //     case 'A':
+            //         gradeDescription = "Excellent";
+            //         break;
+            //     case 'B':
+            //         gradeDescription = "Good";
+            //         break;
+            //     case 'C':
+            //         gradeDescription = "Average";
+            //         break;
+            //     case 'F':
+            //         gradeDescription = "Fail";
+            //         break;
+            //     default:
+            //         gradeDescription = "None";
+            //         break;
+
+
+            // }
+            // System.Console.WriteLine(gradeDescription);
+
+            //try-catch
+
+            // int x = 0;
+            // int div = 0;
+            // try
+            // {
+            //     div = 100 / x;
+            //     Console.WriteLine("Not executed line");
+            // }
+            // catch
+            // {
+            //     Console.WriteLine("Exception");
+            // }
+            // Console.WriteLine($"Result is {div}");
+
+            //throw
+
+            // int age;
+            // age=Convert.ToInt32(Console.ReadLine());
+            // if(age<0){
+            //     throw new Exception("Cannot be defined");
+            // }
+            // else{
+            //     Console.WriteLine("your age is :"+age);
+            // }
+
+
+            // int n1 = 0, n2 = 1, n3=2, i, number;
+            // Console.Write("Enter the number of elements: ");
+             //number = int.Parse(Console.ReadLine()!);
+            // Console.Write(n1 + " " + n2 + " "+n3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
